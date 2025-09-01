78c0c9777a8a9b2924842c9ac872e9847561ed6c
savad k <124030718+S4vad@users.noreply.github.com>
2025-09-01 11:28:55 +0530

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import AuthProvider from "../app/providers/Session-provider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
